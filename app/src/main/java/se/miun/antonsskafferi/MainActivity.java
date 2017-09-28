66ff6658dd7a5435169299b133c87faa836515d1
JoanneBerg <myberg@live.com>
2017-09-28 21:27:15 +0200

    "Meny" för alla rätter.
    Scroller listview, med en counter för att addera dem.

--- app/src/main/java/se/miun/antonsskafferi/MainActivity.java
+++ app/src/main/java/se/miun/antonsskafferi/MainActivity.java
@@ -1,6 +1,7 @@
 package se.miun.antonsskafferi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CoursesActivity.class);
+        startActivity(intent);
     }
 }
