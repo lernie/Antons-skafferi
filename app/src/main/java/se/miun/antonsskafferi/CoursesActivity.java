9b31e56e2caae7758fb6e9e051c48cd54f730a13
My <myberg@live.com>
2017-09-30 16:22:47 +0200

    CoursesActivity funkar typ (Special).
     Lade till en Special knapp.

--- app/src/main/java/se/miun/antonsskafferi/CoursesActivity.java
+++ app/src/main/java/se/miun/antonsskafferi/CoursesActivity.java
@@ -2,7 +2,10 @@ package se.miun.antonsskafferi;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +18,7 @@ public class CoursesActivity extends Activity{
     ListView listView;
     ArrayList<CourseListItem> list = new ArrayList<CourseListItem>();
     CourseAdapter userAdapter;
+    private Button spec;
 
 
     @Override
@@ -35,7 +39,14 @@ public class CoursesActivity extends Activity{
         listView = (ListView) findViewById(R.id.list_View);
         listView.setItemsCanFocus(false);
         listView.setAdapter(userAdapter);
+        spec = (Button)findViewById(R.id.Special);
 
+
+        }
+
+        public void toast(View v){
+            Toast.makeText(CoursesActivity.this,
+                    "Special Added", Toast.LENGTH_LONG).show();
 
         }
 
