1205bb16a400661da77ae6937fd234efc1212486
Nethusan Chandrakumar <nethusan.speed@gmail.com>
2017-10-05 16:43:48 +0200

    fix

--- backend/src/main/java/se/miun/antonsskafferi/TokenAuthenticationService.java
+++ backend/src/main/java/se/miun/antonsskafferi/TokenAuthenticationService.java
@@ -1,4 +1,20 @@
 package se.miun.antonsskafferi;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security
+        .authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+import static java.util.Collections.emptyList;
+
 public class TokenAuthenticationService {
+    static final long EXPIRATIONTIME = 864_000_000; // 10 days
+    static final String SECRET = "ThisIsSecret";
+    static final String TOKEN_PREFIX = "Bearer";
+    static final String HEADER_STRING = "Authorization";
 }
