163821105ba317c41f2d4a5b469a4bb1f0239f0b
JoanneBerg <myberg@live.com>
2017-10-11 16:06:01 +0200

    Läser in Courses från backend.
    Fixat så att retrofit funkar och rätterna läses in från urln.

--- app/src/main/java/se/miun/antonsskafferi/CoursesActivity.java
+++ app/src/main/java/se/miun/antonsskafferi/CoursesActivity.java
@@ -2,7 +2,6 @@ package se.miun.antonsskafferi;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
