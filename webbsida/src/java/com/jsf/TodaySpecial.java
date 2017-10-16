a378fe75be4e94b05799ddac99ffd1fb87f29650
lernie <male1500@student.miun.se>
2017-10-16 20:14:33 +0200

    Mer scriptskit

--- webbsida/src/java/com/jsf/TodaySpecial.java
+++ webbsida/src/java/com/jsf/TodaySpecial.java
@@ -6,9 +6,11 @@
 package com.jsf;
 
 import java.io.Serializable;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped; 
 
@@ -18,6 +20,12 @@ public class TodaySpecial implements Serializable{
 
     private String todaysDate = null;
     private int dayOfTheWeek = 0;
+    private String currentMon = null;
+    private String currentTue = null;
+    private String currentWed = null;
+    private String currentThu = null;
+    private String currentFri = null;
+
     
 
 
@@ -27,14 +35,25 @@ public TodaySpecial() {
     /**
      * @param todaysDate
      * @param dayOfTheWeek
+     * @param currentMon
+     * @param currentTue
+     * @param currentWed
+     * @param currentThu
+     * @param currentFri
      */
 }
 
-public TodaySpecial(String todaysDate, int dayOfTheWeek) {
+public TodaySpecial(String todaysDate, int dayOfTheWeek, String currentMon, String currentTue, String currentWed, String currentThu, String currentFri) {
 
 
     this.todaysDate = todaysDate;
     this.dayOfTheWeek = dayOfTheWeek;
+    this.currentMon = currentMon;
+    this.currentTue = currentTue;
+    this.currentWed = currentWed;
+    this.currentThu = currentThu;
+    this.currentFri = currentFri;
+
     
 }
 
@@ -60,5 +79,75 @@ public int getDayOfTheWeek(){
 
 public void setDayOfTheWeek(int dayOfTheWeek){
     this.dayOfTheWeek = dayOfTheWeek;
+}
+
+public String getCurrentMon(){
+    Calendar c = Calendar.getInstance();
+    c.setFirstDayOfWeek(Calendar.MONDAY);
+    c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    currentMon = df.format(c.getTime());
+
+    return currentMon;
+}
+
+public void setCurrentMon(String currentMon){
+    this.currentMon = currentMon;
+}
+
+public String getCurrentTue(){
+    Calendar c = Calendar.getInstance();
+    c.setFirstDayOfWeek(Calendar.MONDAY);
+    c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    c.add(Calendar.DATE, 1);
+    currentTue = df.format(c.getTime());
+    return currentTue;
+}
+
+public void setCurrentTue(String currentTue){
+    this.currentTue = currentTue;
+}
+
+public String getCurrentWed(){
+    Calendar c = Calendar.getInstance();
+    c.setFirstDayOfWeek(Calendar.MONDAY);
+    c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    c.add(Calendar.DATE, 2);
+    currentWed = df.format(c.getTime());
+    return currentWed;
+}
+
+public void setCurrentWed(String currentWed){
+    this.currentWed = currentWed;
+}
+
+public String getCurrentThu(){
+    Calendar c = Calendar.getInstance();
+    c.setFirstDayOfWeek(Calendar.MONDAY);
+    c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    c.add(Calendar.DATE, 3);
+    currentThu = df.format(c.getTime());
+    return currentThu;
+}
+
+public void setCurrentThu(String currentThu){
+    this.currentThu = currentThu;
+}
+
+public String getCurrentFri(){
+    Calendar c = Calendar.getInstance();
+    c.setFirstDayOfWeek(Calendar.MONDAY);
+    c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    c.add(Calendar.DATE, 4);
+    currentFri = df.format(c.getTime());
+    return currentFri;
+}
+
+public void setCurrentFri(String currentFri){
+    this.currentFri = currentFri;
 }
 }
---
+++ webbsida/web/resources/javascript/script.js
@@ -0,0 +1,243 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function getLunch(d1,d2,d3,d4,d5){
+    
+    var date1 = d1; //Today or Monday
+    var date2 = d2; //Today or Friday
+    var date3 = d3; //Tuesday
+    var date4 = d4; //Wedenesday
+    var date5 = d5; //Thursday
+    
+    if(date1 === date2){
+        $.ajax({
+        //url: "http://simonarstam.com/antons-skafferi/api/todayslunch?startdate=2017-11-02&enddate=2017-11-05",
+        url: "http://simonarstam.com/antons-skafferi/api/todayslunch?startdate="+d1+"&enddate="+d2,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success : function(r) {
+            
+            
+                for(var i = 0; i < r.length; i++){
+                   
+                    $("#section1").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+                }
+            
+        }
+    });
+    }
+    else{
+    
+    
+    $.ajax({
+        //url: "http://simonarstam.com/antons-skafferi/api/todayslunch?startdate=2017-11-02&enddate=2017-11-05",
+        url: "http://simonarstam.com/antons-skafferi/api/todayslunch?startdate="+date1+"&enddate="+date2,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success : function(r) {
+            
+                for(var i = 0; i < r.length; i++){
+                    
+                    var date = r[i].todaysDate;
+                    
+                    //MONDAY
+                    if (date1 === date){ 
+                        $("#section2").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+
+                    }
+                    
+                    //TUESDAY
+                    else if(date3 === date){ 
+                        $("#section2").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+                    }
+                    
+                    //WEDNESDAY
+                    else if(date4 === date){
+                        $("#section2").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+                    }
+                    
+                    //THURSDAY
+                    else if(date5 === date){
+                        $("#section2").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+                    }
+                    
+                    // FRIDAY
+                    else if(date2 === date){ 
+                        $("#section2").append("<b>Dagens lunch:</b> ",r[i].foodName," ",r[i].price,":-"," ",r[i].todaysDate,"<br>");
+                    }
+                    else {
+                        
+                    }
+                    console.log(date);
+
+                   
+                    
+                    
+                   
+                };
+        
+        }
+    });
+    }
+
+}
+function getTodaySpecials(day, date){
+         
+    switch(day) {
+             
+    case 1: //Sunday
+        $("#day").val("Söndag = Stängt")
+        break;
+        
+    case 2: //Monday
+        getLunch(date, date);
+        
+        $("#day").val("Måndag")
+            
+        break;
+        
+    case 3: //Tuesday
+        getLunch(date, date);
+        $("#day").val("Tisdag")
+        break;
+        
+    case 4: //Wednesday
+        getLunch(date, date);  
+        $("#day").val("Onsdag")
+        break;
+        
+    case 5: //Thursday
+        getLunch(date, date);
+        $("#day").val("Torsdag")
+        break;
+        
+    case 6: //Friday
+        getLunch(date, date);
+        $("#day").val("Fredag")
+        break;
+        
+    case 7: //Saturday
+        $("#day").val("Lördag är lunchstängt!");
+        break;
+}
+
+};
+
+function loadDropDown(){
+
+           $.getJSON("http://simonarstam.com/antons-skafferi/api/food", function(r){
+          $("#foodDropDown").html("");
+          
+         
+         $("#foodDropDown").append("<option selected='true' disabled='disabled'>Choose Food:</option>");
+       for(var i = 0; i < r.length; i++){
+            
+            $("#foodDropDown").append("<option value='"+r[i].id+"'>"+r[i].name+"</option>");
+            }
+            
+            
+        
+   });    
+};
+/*
+$("#fillMe").on('change', function(){
+            $("#foodTxt").val($('#fillMe').val());
+            
+        });
+*/
+
+
+function getDatePicker(){
+        $("#datePicker").datepicker({ dateFormat: 'yy-mm-dd' }).datepicker( "show")
+    
+};
+
+function sendTodayToServer() {
+         //  $("#success").val(JSON.stringify({foodId:$('#foodDropDown').val(),todaysDate:$('#datePicker').val()}));
+          
+            /*    $.ajax({
+                    url: "http://simonarstam.com/antons-skafferi/api/todayslunch",
+                    method: "POST",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    data : JSON.stringify({foodId:$('#foodDropDown').val(),todaysDate:$('#datePicker').val(),price:$('#foodPrice').val()}),
+                    //data : JSON.stringify({"foodId": 47, "todaysDate": "2017-10-15"}),
+                    
+                    success : function(r) {
+                        console.log(r);
+                    }
+                })*/
+                $('#foodDropDown').val(function() {
+                    return this.defaultValue;
+                });
+                $('#datePicker').val(function() {
+                    return this.defaultValue;
+                });
+                $('#foodPrice').val(function() {
+                    return this.defaultValue;
+                });
+};
+
+function deleteLunch(){
+    var url = "http://simonarstam.com/antons-skafferi/api/todayslunch/"+$("#datePicker").val()+"/"+$("#foodDropDown").val();
+    
+        $.ajax({
+              
+            url: url,
+            method : "DELETE",
+            success : function(r) {
+                console.log(r);
+          }
+      });
+}
+   
+/*
+$(document).ready(function(){
+            
+            $("#button").click(function() {
+                $.ajax({
+                    url: "http://simonarstam.com/antons-skafferi/api/food",
+                    method: "POST",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    data : JSON.stringify({name:$("#name").val(),foodTypeId: 1,timeToCook:$("#tid").val(),price:$("#pris").val()}),
+                    success : function(r) {
+                       // console.log(r);
+                    }
+                }).done(function(response) {
+                    
+                   
+                });
+            });
+            
+            $.ajax({
+                    url: "http://simonarstam.com/antons-skafferi/api/food",
+                    method: "GET",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success : function(r) {
+                       // console.log(r);
+                        //<![CDATA[
+                        for(var i = 0; i < r.length; i++){
+                           console.log(r[i]);
+                           $("#section3").append("<b>Inventarie ID:</b> ",r[i].id," <b>Maträtt:</b> ",r[i].name," <b>FoodID:</b> ",r[i].foodTypeId," <b>Tillagningstid:</b> ",r[i].timeToCook," <b>Pris:</b> ",r[i].price,"<br>");
+                        }
+                       
+                       //]]>
+                        r.forEach(function(element) {
+                        
+                    });
+                    }
+                }).done(function(response) {
+                    
+                
+                }); 
+                
+});
+*/
