7db1f6ca75366f15941f7a31fe32cacdb2dfe5df
My <myberg@live.com>
2017-10-16 11:50:18 +0200

    Enheterna läses från db nu.
    Fixat så att enheter läses in från db samt inventering fix.

--- app/src/main/java/se/miun/antonsskafferi/IngredientPopupWindow.java
+++ app/src/main/java/se/miun/antonsskafferi/IngredientPopupWindow.java
@@ -1,6 +1,7 @@
 package se.miun.antonsskafferi;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.PopupWindow;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
@@ -23,16 +25,14 @@ public class IngredientPopupWindow extends PopupWindow{
     private PopupWindow popupWindow;
     private int id;
 
-    IngredientPopupWindow(Activity parent, int id){
+    IngredientPopupWindow(Activity parent){
 
         // get a reference to the already created main layout
-        LinearLayout mainLayout = (LinearLayout) parent.findViewById(R.id.inventory_layout);
+        final LinearLayout mainLayout = (LinearLayout) parent.findViewById(R.id.inventory_layout);
 
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater) parent.getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView;
-
-
 
         if(parent.findViewById(R.id.add_ingredient).isPressed()) {
             popupView = inflater.inflate(R.layout.inventory_popup, null);
@@ -46,35 +46,32 @@ public class IngredientPopupWindow extends PopupWindow{
         boolean focusable = false; // lets taps outside the popup also dismiss it
         popupWindow = new PopupWindow(popupView, width, height, focusable);
 
-        ArrayList<UnitService.Unit> unitsList = new ArrayList<UnitService.Unit>();
-        unitsList.add(new UnitService.Unit());
-   /*     units.add("st");
-        units.add("l");
-        units.add("dl");
-        units.add("cl");
-        units.add("ml");
-        units.add("kg");
-        units.add("hg");
-        units.add("g");*/
+        final ArrayList<String> unitsList = new ArrayList<String>();
 
-
-        ArrayAdapter<UnitService.Unit> dataAdapter = new ArrayAdapter<UnitService.Unit>(parent, android.R.layout.simple_spinner_item, id);
+        final ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(parent, android.R.layout.simple_spinner_item, unitsList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) popupView.findViewById(R.id.popup_spinner_unit);
         spinner.setAdapter(dataAdapter);
 
+        UnitCache.getInstance().update(new UnitCache.UpdateCallback() {
+          @Override
+                  public void onSuccess() {
+              HashMap<Integer, String> values = UnitCache.getInstance().getUnits();
 
+                 for (String unit : UnitCache.getInstance().getUnits().values()) {
+                      unitsList.add(unit);
+                  }
 
-        // show the popup window
-        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-    }
+                dataAdapter.notifyDataSetChanged();
+              popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
 
-    public int getId() {
-        return id;
-    }
+          }
 
-    public void setId(int id) {
-        this.id = id;
+            public void onFail() {
+                unitsList.clear();
+                dataAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     public void remove() {
