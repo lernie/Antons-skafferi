726822226084dab8a6a4e0861867c562fdecdcb2
WictorSvensson <wictor110@hotmail.com>
2017-10-16 20:13:02 +0200

    Påbörjan av schemat, mest krigat i xml

---
+++ app/src/main/java/se/miun/antonsskafferi/EmployeeScheduleActivity.java
@@ -0,0 +1,13 @@
+package se.miun.antonsskafferi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeScheduleActivity extends NavigationActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_schedule);
+    }
+}
