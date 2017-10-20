4856545a39dbc5a2a8f231e52f96ad19a2a6ae77
WictorSvensson <wictor110@hotmail.com>
2017-10-20 16:53:02 +0200

    Fixade "+" knapper i inventeringen.
    Lade även in inventoryserviceitem i inventoryservice

--- app/src/main/java/se/miun/antonsskafferi/InventoryService.java
+++ app/src/main/java/se/miun/antonsskafferi/InventoryService.java
@@ -12,4 +12,19 @@ import retrofit2.http.GET;
 public interface InventoryService {
     @GET("inventory")
     Call<List<InventoryServiceItem>> getInventory();
+
+    /**
+     * Created by joel on 2017-10-10.
+     */
+
+    class InventoryServiceItem {
+        private int ingredientId;
+        private float amount;
+
+        public int getIngredientId() { return ingredientId; }
+
+        public float getAmount() {
+            return amount;
+        }
+    }
 }
