b46f73919fab16754c620d975eb45409c58405cd
My <myberg@live.com>
2017-10-21 12:30:16 +0200

    RegisteringActivity på G
    RegActivity på G med layout klar

--- app/src/main/java/se/miun/antonsskafferi/RegisterActivity.java
+++ app/src/main/java/se/miun/antonsskafferi/RegisterActivity.java
@@ -1,5 +1,6 @@
 package se.miun.antonsskafferi;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
 
 /**
  * Created by My on 10/19/2017.
@@ -25,48 +30,53 @@ public class RegisterActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        email = (EditText) findViewById(R.id.email_login);
-        password = (EditText) findViewById(R.id.password);
-        passwordCheck = (EditText) findViewById(R.id.password_check);
-        button_register = (Button) findViewById(R.id.reg_btn);
-
-        button_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mail = email.getText().toString();
-                pword = password.getText().toString();
-                pcheck = passwordCheck.getText().toString();
-
-                if(mail.equals("") || pword.equals(" ") || pcheck.equals(" ")) {
-                    Toast.makeText(RegisterActivity.this, "u fked up", Toast.LENGTH_LONG).show();
-                return;
-                }
-
-                if(pword != pcheck) {
-                    Toast.makeText(RegisterActivity.this, "not same password", Toast.LENGTH_LONG).show();
-                return;
-                }
-
-                if(mail.length() <= 1  || pword.length() <= 1 || pcheck.length() <= 1){
-                    Toast.makeText(RegisterActivity.this, "Email or password is to short", Toast.LENGTH_LONG).show();
-               return;
-                }
-
-            }
-        });
     }
+
     public void goToLogin(View v) {
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+      //  if (CheckFieldValidation()) {
+            email = (EditText) findViewById(R.id.email_login);
+            password = (EditText) findViewById(R.id.password);
+            passwordCheck = (EditText) findViewById(R.id.password_check);
+            button_register = (Button) findViewById(R.id.reg_btn);
+
+            LoginCache.getInstance().setUser(new User(email.getText().toString(), password.getText().toString()));
+
+            LoginCache.getInstance().update(new LoginCache.UpdateCallback() {
+                @Override
+                public void onSuccess() {
+
+                    finish();
+                    startActivity(getIntent());
+                    email.setText("");
+                    password.setText("");
+
+
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+
+                @Override
+                public void onFail() {
+                    Toast.makeText(RegisterActivity.this, "Fel lösenord eller mail", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
+/*
+        private boolean CheckFieldValidation() {
+            boolean valid = true;
+            if (email.getText().toString().equals("")) {
+                email.setError("Can't be Empty");
+                valid = false;
+            } else if (password.getText().toString().equals("")) {
+                password.setError("Can't be Empty");
+                valid = false;
+            }
+                return valid;
+        }*/
 
-    public void register() {
 
-        mail = email.getText().toString();
-        pword = password.getText().toString();
-        pcheck = passwordCheck.getText().toString();
 
-    }
-}
